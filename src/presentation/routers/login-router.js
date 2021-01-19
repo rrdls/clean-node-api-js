93a1860e91146ae5a60346a86a69b89fc1b5d996
rrdls <rramosdls.developer@gmail.com>
2021-01-19 00:10:04 -0300

    ensure LoginRouter calls AuthUseCase with correct params

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,11 +1,16 @@
 const HttpResponse = require('../helpers/http-response')
 
 class LoginRouter {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
     const { email, password } = httpRequest.body
     if (!email) return HttpResponse.badRequest('email')
     if (!password) return HttpResponse.badRequest('password')
+    this.authUseCase.auth(email, password)
   }
 }
 
