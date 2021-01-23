43414f9b048a5662fe337494d5e052fe9ed03ece
rrdls <rramosdls.developer@gmail.com>
2021-01-23 03:23:29 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -21,7 +21,7 @@ class LoginRouter {
     if (!accessToken) {
       return HttpResponse.unauthorizedError()
     }
-    return HttpResponse.ok()
+    return HttpResponse.ok({ accessToken })
   }
 }
 
