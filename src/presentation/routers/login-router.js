6db5c0b22dcfb8ab9848c56baa6b3e13ee0387a9
rrdls <rramosdls.developer@gmail.com>
2021-01-23 03:07:07 -0300

    ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -6,7 +6,12 @@ class LoginRouter {
   }
 
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) return HttpResponse.serverError()
+    if (
+      !httpRequest ||
+            !httpRequest.body ||
+            !this.authUseCase ||
+            !this.authUseCase.auth
+    ) { return HttpResponse.serverError() }
     const { email, password } = httpRequest.body
     if (!email) return HttpResponse.badRequest('email')
     if (!password) return HttpResponse.badRequest('password')
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -54,9 +54,40 @@ describe('Login Router', () => {
 
   test('Should return 401 when invalid credentials are provided', () => {
     const { sut } = makeSut()
-    const httpRequest = { body: { email: 'invalid_email@gmail.com', password: 'invalid_password' } }
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@gmail.com',
+        password: 'invalid_password'
+      }
+    }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
+  })
+
+  test('Should return 500 if no authUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should return 500 if AuthUseCase has no auth method', () => {
+    class AuthUseCaseSpy {}
+    const authUseCaseSpy = new AuthUseCaseSpy()
+    const sut = new LoginRouter(authUseCaseSpy)
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
   })
 })
